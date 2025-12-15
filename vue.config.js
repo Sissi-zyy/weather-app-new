4208b48388dd5fa8d13a830f8a9dfde74206ff51
Sissi-zyy <3356329769@qq.com>
2025-12-15 21:54:23 +0800

    修复vue.config.js配置格式

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   publicPath: './', // 关键：设置为相对路径
   outputDir: 'dist', // 确保打包输出到dist文件夹
+  transpileDependencies: {},
   configureWebpack: {
     performance: {
       hints: false
